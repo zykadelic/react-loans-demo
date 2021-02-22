8614a0f5f2e6a8a9cdbf12dc7aa362754832a083
Andreas Fransson <root@zykadelic.com>
2021-02-22 19:14:38 +0100

    implements redux-persist for storing data between page reloads

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+// localStore:
+// import storage from 'redux-persist/lib/storage';
+// sessionStore:
+import storage from 'redux-persist/lib/storage/session';
 import loansReducer from '../components/loans/loansSlice';
 
-export default configureStore({
-  reducer: {
-    loans: loansReducer
-  }
+const rootReducer = combineReducers({
+  loans: loansReducer
 });
+
+const persistConfig = { key: 'root', storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      // https://github.com/rt2zz/redux-persist/issues/988#issuecomment-552242978
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
+})
+const persistor = persistStore(store);
+
+export { store, persistor };
